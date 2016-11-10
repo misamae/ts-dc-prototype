e9970e1a9221f8daf0740deb5a9fc73b7173190b
Meisam Emamjome <misamae@outlook.com>
2016-11-10 19:35:35 +0000

    multi-series with average

--- app.ts
+++ app.ts
@@ -34,14 +34,18 @@ interface KeyGroupKey {
     key: any;
     value: GroupKey;
 }
+
 /***
  * prototype to do a simple dashboard
- * bar chart for imei => average speed
- * bar chart for imei, date => average speed per day
- * bar chart for imei, date => average moving speed per day
- *
- * ideally need to add these
- * line chart per registration number and speed
+ * bar chart points per day => DONE
+ * bar chart for imei average speed => DONE
+ * line chart per imei and speed => DONE
+
+ * organisation filter
+ * Imei filter
+ * registration number filter
+
+ * line chart per imei and average moving speed
  */
 export class App {
 
@@ -74,8 +78,22 @@ export class App {
         let imeiDimension = ndx.dimension((d:TransformedDataPoint) => d.imei);
 
         let signalsCountGroup = dayDimension.group();
-        let speedsSumGroup = dayImeiDimension.group().reduceSum((d: TransformedDataPoint) => d.speed);
         let speedAverageGroup = imeiDimension.group()
+            .reduce((p: GroupKey, v: TransformedDataPoint) => {
+                ++p.number;
+                p.total += +v.speed;
+                p.avg = Math.round(p.total / p.number);
+                return p;
+            },
+                (p: GroupKey, v: TransformedDataPoint) => {
+                --p.number;
+                p.total -= +v.speed;
+                p.avg = (p.number == 0) ? 0 : Math.round(p.total / p.number);
+                return p;
+            },
+                () => { return {number: 0, total: 0, avg: 0};});
+
+        let dailySpeedAverageGroup = dayImeiDimension.group()
             .reduce((p: GroupKey, v: TransformedDataPoint) => {
                 ++p.number;
                 p.total += +v.speed;
@@ -94,7 +112,8 @@ export class App {
         let maxDate = dayDimension.top(1)[0].date;
 
         let signalsBar = dc.barChart("#signals-per-day-chart");
-        let speedLineChart = dc.seriesChart("#speed-per-imei");
+        // let speedLineChart = dc.seriesChart("#speed-per-imei");
+        let averageSpeedLineChart = dc.seriesChart("#average-speed-per-imei");
         let imeiCountBar = dc.barChart("#imei-counts");
 
         signalsBar
@@ -109,24 +128,21 @@ export class App {
             .yAxis()
             .ticks(6);
 
-        speedLineChart
+        averageSpeedLineChart
             .chart(c => dc.lineChart(c))
             .x(d3.time.scale().domain([minDate, maxDate]))
             .brushOn(false)
             .yAxisLabel("Speed km/h")
             .elasticY(true)
             .dimension(dayImeiDimension)
-            .group(speedsSumGroup)
-            // .dimension(speedAverageGroup)
-            // .group()
+            .group(dailySpeedAverageGroup)
             .mouseZoomable(false)
             .seriesAccessor(d => "imei: " + d.key[0])
-            .keyAccessor(d => d.key[1])
-            .valueAccessor(d => d.value)
+            .keyAccessor((d: KeyGroupKey) => d.key[1])
+            .valueAccessor((d: KeyGroupKey) => d.value.avg)
         ;
 
         imeiCountBar
-            // .centerBar(false)
             .dimension(imeiDimension)
             .group(speedAverageGroup)
             .valueAccessor((g: KeyGroupKey) => g.value.avg)
@@ -140,10 +156,6 @@ export class App {
             .ticks(6);
 
         dc.renderAll();
-    }
-
-    extractDate(t: number) {
-        return new Date(t);
     }
 }
 
